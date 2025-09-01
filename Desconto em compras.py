ee050e9bd1ad6bf976a506d97c7e9a44d4f56687
Jean Paulo <jeanpaulolacerda@gmail.com>
2025-08-31 22:35:34 -0300

    agradecimentos

--- Desconto em compras.py
+++ Desconto em compras.py
@@ -10,3 +10,5 @@ elif opcao == 2:
     print('Você escolheu Cartão de crédito em 3x\n sua compra de R${} sairá por R${:.2f}'.format(compra, compra - (compra * 5/100)))
 elif opcao == 3:
     print('Você escolheu Cartão de crédito em 4x ou mais\n essa opção terá juros de 3%\n sua compra de R${} sairá por R${:.2f}'.format(compra, compra + (compra * 3/100)))
+
+print('Obrigado por utilizar nosso sistema')
