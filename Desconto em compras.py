2006639df2fd5650380e21c371d0de85d3da51d1
Jean Paulo <jeanpaulolacerda@gmail.com>
2025-08-31 22:32:31 -0300

    Desconto em compras

---
+++ Desconto em compras.py
@@ -0,0 +1,12 @@
+compra = float(input('Digite o valor de sua compra: R$'))
+print('''Escolha a opção de pagamento.
+[1] - A vista/Pix (desconto de 10%)
+[2] - Cartão de crédito em 3x (desconto de 5%)
+[3] - Cartão de crédito em 4x ou mais (juros de 3%)''')
+opcao = int(input('Digite a opção desejada: '))
+if opcao == 1:
+    print('Você escolheu A vista/Pix\n sua compra de R${:.2f} sairá por R${}'.format(compra, compra - (compra * 10/100)))
+elif opcao == 2:
+    print('Você escolheu Cartão de crédito em 3x\n sua compra de R${} sairá por R${:.2f}'.format(compra, compra - (compra * 5/100)))
+elif opcao == 3:
+    print('Você escolheu Cartão de crédito em 4x ou mais\n essa opção terá juros de 3%\n sua compra de R${} sairá por R${:.2f}'.format(compra, compra + (compra * 3/100)))
